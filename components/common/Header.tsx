e1e6b40773011729fd7db258afd75df66799cecf
mrLuisFer <mrluisfeer@gmail.com>
2021-10-08 22:48:10 -0500

    [chore] first commit

---
+++ components/common/Header.tsx
@@ -0,0 +1,10 @@
+const Header = (): JSX.Element => {
+	return (
+		<div>
+			<p>Some</p>
+		</div>
+	)
+}
+
+
+export default Header
---
+++ components/home/Content.tsx
@@ -0,0 +1,12 @@
+import type {TImages} from '../../types/Images'
+
+const HomeContent = ({images}: TImages) => {
+	console.log(images)
+	return (
+		<div>
+			<p>content</p>
+		</div>
+	)
+}
+
+export default HomeContent
