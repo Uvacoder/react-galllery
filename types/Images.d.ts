07f931175f93a5987bcafe2963d86ab7eff7bd2b
mrLuisFer <mrluisfeer@gmail.com>
2021-10-11 10:59:50 -0500

    [feat] add original data and create new interface for data types

--- types/Images.d.ts
+++ types/Images.d.ts
@@ -6,3 +6,36 @@ export type TImages = {
   url: string
   download_url: string
 }
+
+interface ArtistInterface {
+  image: string
+  name: string
+  artistwidth: string
+  artistheight: string
+}
+
+interface ImagesProperties {
+  gallerywidth: number
+  galleryheight: number
+  bigwidth: number
+  bigheight: number
+  smallwidth: number
+  smallheight: number
+  thumbwidth: number
+  thumbheight: number
+  thumbnail: string
+  hero: {
+    small: string
+    large: string
+  }
+  gallery: string
+}
+
+export interface ImageInterface {
+  name: string
+  year: number
+  description: string
+  source: string
+  artist: ArtistInterface
+  images: ImagesProperties
+}
---
+++ utils/photos.ts
@@ -0,0 +1,60 @@
+import { useState, useCallback, useEffect } from 'react'
+import data from '../db/data.json'
+
+export function getPostData() {
+  const allPostData = []
+
+  data.map((image, index) => {
+    const id = index
+
+    allPostData.push({
+      id,
+      ...image,
+    })
+  })
+
+  return allPostData
+}
+
+export function getAllPostIds() {
+  return data.map((data, index) => {
+    const id = index
+    return {
+      params: {
+        id: id.toString(),
+      },
+    }
+  })
+}
+
+export function getOnePostData(id) {
+  const filtered = data[id]
+
+  return { id, ...filtered }
+}
+
+export function useMediaQuery(width) {
+  const [targetReached, setTargetReached] = useState(false)
+
+  const updateTarget = useCallback((e) => {
+    if (e.matches) {
+      setTargetReached(true)
+    } else {
+      setTargetReached(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width}px)`)
+    media.addListener(updateTarget)
+
+    // Check on mount (callback is not called until a change occurs)
+    if (media.matches) {
+      setTargetReached(true)
+    }
+
+    return () => media.removeListener(updateTarget)
+  }, [])
+
+  return targetReached
+}
